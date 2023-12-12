c17c98f8b81dd9c120dde31cae2ebd65db5ac065
ZenioPC <zenio1026@gmail.com>
2023-12-13 03:10:40 +0800

    Get All Product Ui call w/ api

--- src/api/GetAllProduct.ts
+++ src/api/GetAllProduct.ts
@@ -1,7 +1,6 @@
 import {ProductListDto} from "../data/dto/ProductDto.ts";
 import axios from 'axios';
 
-//?????
 export const getAllProduct = async (): Promise<ProductListDto> => {
     const response = await axios.get<ProductListDto>("http://localhost:8080/public/product")
     return response.data
